4d1db131fbff33e9b20d4e7d9a04b86d1235ea0e
Gyx <2397245326@qq.com>
2023-09-19 16:10:21 +0800

    test

--- cmd/gocq/bot.go
+++ cmd/gocq/bot.go
@@ -1,6 +1,7 @@
 package gocq
 
 import (
+	"bytes"
 	"crypto/md5"
 	"fmt"
 	"github.com/Mrs4s/MiraiGo/binary"
@@ -12,6 +13,8 @@ import (
 	"github.com/Mrs4s/go-cqhttp/modules/servers"
 	log "github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
+	"image"
+	"image/jpeg"
 	"os"
 	"sync"
 	"time"
@@ -23,8 +26,8 @@ func BaseInit() {
 	base.Parse()
 }
 
-func MainByT3(QQAccount int64, QQPassword string, protocol int) {
-	initByT3(QQAccount, QQPassword)
+func RunQQRobot(QQAccount int64, QQPassword string, protocol int) {
+	initAccount(QQAccount, QQPassword)
 
 	if (base.Account.Uin == 0 || (base.Account.Password == "" && !base.Account.Encrypt)) && !global.PathExists("session.token") {
 		log.Warn("账号密码未配置, 将使用二维码登录.")
@@ -65,7 +68,7 @@ func MainByT3(QQAccount int64, QQPassword string, protocol int) {
 					log.Warnf("请选择: (自动选2)")
 					_ = os.Remove("session.token")
 					log.Infof("缓存已删除.")
-					MainByT3(QQAccount, QQPassword, protocol)
+					RunQQRobot(QQAccount, QQPassword, protocol)
 				}
 			}
 			if err = cli.TokenLogin(token); err != nil {
@@ -186,7 +189,7 @@ func MainByT3(QQAccount int64, QQPassword string, protocol int) {
 	servers.Run(CQBot)
 }
 
-func initByT3(account int64, password string) {
+func initAccount(account int64, password string) {
 	accountInfo := config.Account{
 		Uin:      account,  // QQ账号
 		Password: password, // 密码为空时使用扫码登录
@@ -250,74 +253,91 @@ func DeviceInfo(protocol int) string {
 	return fmt.Sprintf(cfg, protocol)
 }
 
+const (
+	ProtocolDefault = 0 // QQ登录方式(Default/Unset)	当前版本下默认为iPad
+	ProtocolAndroid = 1 // QQ登录方式(ndroid Phone)
+	ProtocolWatch   = 2 // QQ登录方式(Android Watch)
+	ProtocolMacOS   = 3 // QQ登录方式(MacOS)
+	ProtocolQiDian  = 4 // QQ登录方式(企点)	只能登录企点账号或企点子账号
+	ProtocolIPad    = 5 // QQ登录方式(iPad)
+	ProtocolAPad    = 6 // QQ登录方式(aPad)
+)
+
+func qrcode() error {
+	rsp, err := cli.FetchQRCodeCustomSize(1, 2, 1)
+	if err != nil {
+		return err
+	}
+	_ = os.WriteFile("qrcode.png", rsp.ImageData, 0o644)
+	defer func() { _ = os.Remove("qrcode.png") }()
+	if cli.Uin != 0 {
+		log.Infof("请使用账号 %v 登录手机QQ扫描二维码 (qrcode.png) : ", cli.Uin)
+	} else {
+		log.Infof("请使用手机QQ扫描二维码 (qrcode.png) : ")
+	}
+	time.Sleep(time.Second)
+	serveFrames(rsp.ImageData)
+	printQRCode(rsp.ImageData)
+	s, err := cli.QueryQRCodeStatus(rsp.Sig)
+	if err != nil {
+		return err
+	}
+	prevState := s.State
+	for {
+		time.Sleep(time.Second)
+		s, _ = cli.QueryQRCodeStatus(rsp.Sig)
+		if s == nil {
+			continue
+		}
+		if prevState == s.State {
+			continue
+		}
+		prevState = s.State
+		switch s.State {
+		case client.QRCodeCanceled:
+			log.Fatalf("扫码被用户取消.")
+		case client.QRCodeTimeout:
+			log.Fatalf("二维码过期")
+		case client.QRCodeWaitingForConfirm:
+			log.Infof("扫码成功, 请在手机端确认登录.")
+		case client.QRCodeConfirmed:
+			res, err := cli.QRCodeLogin(s.LoginInfo)
+			if err != nil {
+				return err
+			}
+			return loginResponseProcessor(res)
+		case client.QRCodeImageFetch, client.QRCodeWaitingForScan:
+			// ignore
+		}
+	}
+}
+
+func serveFrames(imgByte []byte) {
+
+	img, _, err := image.Decode(bytes.NewReader(imgByte))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	out, _ := os.Create(TmpQRCodeIMGPath)
+	defer out.Close()
+
+	var opts jpeg.Options
+	opts.Quality = 1
+
+	err = jpeg.Encode(out, img, &opts)
+	//jpeg.Encode(out, img, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+const TmpQRCodeIMGPath = "./QRCode.jpeg"
+
 func QQMessageSend(targetQQ int64, message string) {
 	CQBot.CQSendPrivateMessage(targetQQ, 0, gjson.Result{
 		Type: 3,
 		Str:  message,
 	}, false)
-}
-
-// ---------- QRCode login ------------
-
-func keepLogin() {
-	var times uint = 1 // 重试次数
-	var reLoginLock sync.Mutex
-	cli.DisconnectedEvent.Subscribe(func(q *client.QQClient, e *client.ClientDisconnectedEvent) {
-		reLoginLock.Lock()
-		defer reLoginLock.Unlock()
-		times = 1
-		if cli.Online.Load() {
-			return
-		}
-		log.Warnf("Bot已离线: %v", e.Message)
-		time.Sleep(time.Second * time.Duration(base.Reconnect.Delay))
-		for {
-			if base.Reconnect.Disabled {
-				log.Warnf("未启用自动重连, 将退出.")
-				os.Exit(1)
-			}
-			if times > base.Reconnect.MaxTimes && base.Reconnect.MaxTimes != 0 {
-				log.Fatalf("Bot重连次数超过限制, 停止")
-			}
-			times++
-			if base.Reconnect.Interval > 0 {
-				log.Warnf("将在 %v 秒后尝试重连. 重连次数：%v/%v", base.Reconnect.Interval, times, base.Reconnect.MaxTimes)
-				time.Sleep(time.Second * time.Duration(base.Reconnect.Interval))
-			} else {
-				time.Sleep(time.Second)
-			}
-			if cli.Online.Load() {
-				log.Infof("登录已完成")
-				break
-			}
-			log.Warnf("尝试重连...")
-			err := cli.TokenLogin(base.AccountToken)
-			if err == nil {
-				return
-			}
-			log.Warnf("快速重连失败: %v", err)
-			log.Warnf("快速重连失败, 尝试普通登录. 这可能是因为其他端强行T下线导致的.")
-			time.Sleep(time.Second)
-			if err := commonLogin(); err != nil {
-				log.Errorf("登录时发生致命错误: %v", err)
-			} else {
-				break
-			}
-		}
-	})
-	cli.AllowSlider = true
-	log.Infof("登录成功 欢迎使用: %v", cli.Nickname)
-	log.Info("开始加载好友列表...")
-	global.Check(cli.ReloadFriendList(), true)
-	log.Infof("共加载 %v 个好友.", len(cli.FriendList))
-	log.Infof("开始加载群列表...")
-	global.Check(cli.ReloadGroupList(), true)
-	log.Infof("共加载 %v 个群.", len(cli.GroupList))
-	if uint(base.Account.Status) >= uint(len(allowStatus)) {
-		base.Account.Status = 0
-	}
-	cli.SetOnlineStatus(allowStatus[base.Account.Status])
-
-	CQBot = coolq.NewQQBot(cli)
-	servers.Run(CQBot)
 }
